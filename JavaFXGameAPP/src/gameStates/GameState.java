cd304285fbd26575bfdad389526713d8b582b4ff
plamenpetkov8 <plamen_petkov_92@abv.bg>
2016-04-15 15:29:00 +0300

    Added a package named "Weapons" that has three classes in it - Towers,Crossbow and Weapons. The Towers and Crossbow classes extend the Weapons class

--- JavaFXGameAPP/src/gameStates/GameState.java
+++ JavaFXGameAPP/src/gameStates/GameState.java
@@ -1,9 +1,11 @@
 package gameStates;
 
 import display.Display;
+import gameObjects.Weapons.Crossbow;
+import gameObjects.Weapons.Towers;
 import gameObjects.castle.CastleWall;
 import graphics.Assets;
-import gameObjects.enemy.Tauren;
+import gameObjects.Enemy.Tauren;
 import java.awt.*;
 import java.awt.image.BufferStrategy;
 
@@ -15,6 +17,8 @@ public class GameState extends State {
 
     private Tauren tauren = new Tauren();
     private CastleWall wall = new CastleWall();
+    private Towers tower = new Towers();
+    private Crossbow crossbow = new Crossbow();
 
     public GameState(Display display,BufferStrategy bufferStrategy,Graphics graphics) {
         this.display = display;
@@ -27,7 +31,8 @@ public class GameState extends State {
     public void tick() {
         wall.tick();
         tauren.tick();
-
+        tower.tick();
+        crossbow.tick();
     }
 
     @Override
@@ -45,6 +50,9 @@ public class GameState extends State {
         graphics.drawImage(Assets.gameBackground, 0, 0, 1280, 720, null);
         wall.render(this.graphics);
         tauren.render(this.graphics);
+        tower.render(this.graphics);
+        crossbow.render(this.graphics);
+
         //End drawing
 
         graphics.dispose();
--- JavaFXGameAPP/src/graphics/Assets.java
+++ JavaFXGameAPP/src/graphics/Assets.java
@@ -10,6 +10,8 @@ public class Assets {
     public static BufferedImage menuBackground;
     public static BufferedImage castleWall;
     public static SpriteSheet tauren;
+    public static BufferedImage tower;
+    public static BufferedImage crossbow;
 
     public static void init(){
         //we load images by putting the name of the folder as a string, then File.separator, then the name of the image
@@ -17,6 +19,8 @@ public class Assets {
         menuBackground = ImageLoader.loadImage("resources" + File.separator + "background.jpg");
         castleWall = ImageLoader.loadImage("resources" + File.separator + "CastleWall_82x575.png");
         tauren = new SpriteSheet(ImageLoader.loadImage("resources" + File.separator + "Tauren_100x86.png"));
+        tower = ImageLoader.loadImage("resources" + File.separator + "Tower_86x256.png");
+        crossbow = ImageLoader.loadImage("resources" + File.separator + "Crossbow_164 x158.png");
     }
 
 
