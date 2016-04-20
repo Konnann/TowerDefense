fa68e722c2c2eb1f365a6d571a2c24fdaa94501c
StefanKaradjov <stefan_karadjov@mail.bg>
2016-04-20 23:27:26 +0300

    +MouseMotionListener

--- JavaFXGameAPP/src/game/MouseMoving.java
+++ JavaFXGameAPP/src/game/MouseMoving.java
@@ -8,8 +8,8 @@ import java.awt.event.MouseMotionListener;
 public class MouseMoving implements MouseMotionListener {
     private static double imageAngleRad = 0;
 
-    private static double x = 0;
-    private static double y = 0;
+    public static double x = 0;
+    public static double y = 0;
 
 
     public MouseMoving(){
@@ -20,18 +20,7 @@ public class MouseMoving implements MouseMotionListener {
 
     }
 
-    //@Override
-    //public void mouseMoved(MouseEvent e) {
-    //    dx = e.getX() - Crossbow.getX();
-    //    dy = e.getY() - Crossbow.getY();
-    //    imageAngleRad = Math.atan2(dy, dx);
-    //}
 
-    //public static double getImageAngleRad() {
-    //    return imageAngleRad;
-    //}
-
-    @Override
     public void mouseMoved(MouseEvent e) {
         x = e.getX();
         y = e.getY();
@@ -41,6 +30,6 @@ public class MouseMoving implements MouseMotionListener {
         double dx = x - crossbowX;
         double dy = y - crossbowY;
         imageAngleRad = Math.atan2(dy, dx);
-        return imageAngleRad;
+        return dy;
     }
 }
